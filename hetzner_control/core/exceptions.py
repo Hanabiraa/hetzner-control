820a7942ff774daecbc9d68db0a558646b777d32
Hanabiraa <workflow.elec@gmail.com>
2022-03-20 00:07:23 +0300

    feat/add documentation

--- hetzner_control/core/exceptions.py
+++ hetzner_control/core/exceptions.py
@@ -2,12 +2,22 @@ from rich.console import Console, Text
 
 
 class BaseExceptionHandler(Exception):
+    """
+    Basic abstract class for custom exception
+    """
     @staticmethod
     def terminate_program() -> None:
+        """
+        fast terminate.
+        Purpose - for hide traceback
+        """
         exit()
 
 
 class ExMessageHandler(BaseExceptionHandler):
+    """
+    Exception class, which print in console rich.console.Text object
+    """
     def __init__(self, message: Text, terminate_after: bool = False):
         self.message = message
         self.__error_message()
