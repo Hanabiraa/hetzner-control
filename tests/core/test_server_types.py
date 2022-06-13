eea643083b4f38f57faca678b02491407a306a1d
Hanabiraa <workflow.elec@gmail.com>
2022-06-13 18:19:34 +0300

    feat/add test for ServerTypesHandler class methods for good/bad response

---
+++ tests/core/test_server_types.py
@@ -0,0 +1,102 @@
+import os
+from unittest import mock
+
+import pytest
+import responses
+from responses import matchers
+
+from hetzner_control.core.server_types import ServerTypesHandler
+
+
+@pytest.fixture(autouse=True)
+def mock_settings_env_vars():
+    """
+    Mock real environment variable
+    """
+    with mock.patch.dict(os.environ, {"HETZNER_API_TOKEN": "1111"}):
+        yield
+
+
+class TestGetAllServerTypes:
+    """
+    For test ServerTypesHandler.get_all_server_types() method for good/bad response
+    """
+    url = "https://api.hetzner.cloud/v1/server_types"
+    headers = {
+        "Authorization": "Bearer 1111",
+    }
+
+    @responses.activate
+    def test_good_status(self):
+        responses.add(
+            method=responses.GET,
+            url=self.url,
+            json={"error": "not found"},
+            status=200,
+            content_type="application/json",
+            match=[
+                matchers.header_matcher(self.headers)
+            ]
+        )
+
+        resp = ServerTypesHandler().get_all_server_types()
+        assert resp["error"] == "not found"
+
+    @responses.activate
+    def test_bad_status(self):
+        responses.add(
+            method=responses.GET,
+            url=self.url,
+            json={"error": {"message": "bad status"}},
+            status=500,
+            content_type="application/json",
+            match=[
+                matchers.header_matcher(self.headers)
+            ]
+        )
+
+        with pytest.raises(SystemExit):
+            _ = ServerTypesHandler().get_all_server_types()
+
+
+class TestGetServerType:
+    """
+    For test ServerTypesHandler.get_server_type() method for good/bad response
+    """
+    id = 111
+    url = f"https://api.hetzner.cloud/v1/server_types/{id}"
+    headers = {
+        "Authorization": "Bearer 1111",
+    }
+
+    @responses.activate
+    def test_good_status(self):
+        responses.add(
+            method=responses.GET,
+            url=self.url,
+            json={"error": "not found"},
+            status=200,
+            content_type="application/json",
+            match=[
+                matchers.header_matcher(self.headers)
+            ]
+        )
+
+        resp = ServerTypesHandler().get_server_type(self.id)
+        assert resp["error"] == "not found"
+
+    @responses.activate
+    def test_bad_status(self):
+        responses.add(
+            method=responses.GET,
+            url=self.url,
+            json={"error": {"message": "bad status"}},
+            status=500,
+            content_type="application/json",
+            match=[
+                matchers.header_matcher(self.headers)
+            ]
+        )
+
+        with pytest.raises(SystemExit):
+            _ = ServerTypesHandler().get_server_type(self.id)
