451123f788de719c01d1352a0f5008d53e0f8d7f
Hanabiraa <workflow.elec@gmail.com>
2022-06-24 21:28:47 +0300

    feat/change uncomment commented code

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,7 +6,7 @@ from hetzner_control.cli import app
 runner = CliRunner()
 
 
-# class TestCli:
-#     def test_app_version(self):
-#         result = runner.invoke(app, ["version"])
-#         assert f"{__app_name__} {__version__}" in result.stdout
+class TestCli:
+    def test_app_version(self):
+        result = runner.invoke(app, ["version"])
+        assert f"{__app_name__} {__version__}" in result.stdout
